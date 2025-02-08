f2211849024008372aa497d23efd8222bdfdb5f6
mugenggeng@163.com <mugenggeng@163.com>
2025-02-08 15:54:27 +0800

    fusion

--- src/rekognition_online_action_detection/engines/base_trainers/perframe_det_trainer.py
+++ src/rekognition_online_action_detection/engines/base_trainers/perframe_det_trainer.py
@@ -257,12 +257,12 @@ def do_perframe_det_train(cfg,
                         # scaler.scale(det_loss).backward()
                         # scaler.step(optimizer)
                         # scaler.update()
-                        # clip_value = 10.0  # 设置裁剪阈值
-                        # torch.nn.utils.clip_grad_norm_(model.parameters(), clip_value)
-                        for name, param in model.named_parameters():
-                            if 'classifier' in name:
-                                # print('111')
-                                torch.nn.utils.clip_grad_norm_(param, 10.0)
+                        clip_value = 5.0  # 设置裁剪阈值
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), clip_value)
+                        # for name, param in model.named_parameters():
+                        #     if 'classifier' in name:
+                        #         # print('111')
+                        #         torch.nn.utils.clip_grad_norm_(param, 10.0)
                         optimizer.step()
                         ema.update()
                         scheduler.step()
