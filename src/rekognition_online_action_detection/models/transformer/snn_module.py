f5a9832f259f3cbc7ba77a0468e2721164304d3d
mugenggeng@163.com <mugenggeng@163.com>
2025-02-10 20:32:31 +0800

    fusion

--- src/rekognition_online_action_detection/models/transformer/snn_module.py
+++ src/rekognition_online_action_detection/models/transformer/snn_module.py
@@ -182,7 +182,8 @@ class RepConv(nn.Module):
 class LearnableLIFNode(MultiStepLIFNode):
     def __init__(self, tau=2.0, device=None,**kwargs):
         super().__init__(tau, **kwargs)
-        self.v_threshold = nn.Parameter(torch.tensor(1.0)).cpu()  # 可学习阈值
+        self.v_threshold_learn = nn.Parameter(torch.tensor(1.0)).to('cuda')  # 可学习阈值
+        self.v_threshold = self.v_threshold_learn.cpu().detch()
 
     def extra_repr(self):
         return f"v_threshold={self.v_threshold.item():.2f}, tau={self.tau}"
