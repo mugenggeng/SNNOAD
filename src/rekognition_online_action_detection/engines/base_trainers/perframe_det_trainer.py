cb8b4f2aa5edb73313997a39b698c0750ae05b43
mugenggeng@163.com <mugenggeng@163.com>
2025-02-11 10:35:47 +0800

    fusion

--- src/rekognition_online_action_detection/engines/base_trainers/perframe_det_trainer.py
+++ src/rekognition_online_action_detection/engines/base_trainers/perframe_det_trainer.py
@@ -124,8 +124,8 @@ def do_perframe_det_train(cfg,
         fut_log = {phase: 0.0 for phase in cfg.SOLVER.PHASES}
         det_pred_scores = []
         det_gt_targets = []
-        print(model)
-        print(hasattr(model, "T"))
+        # print(model)
+        print(hasattr(model, 'T'))
         if hasattr(model, "T"):
             if epoch < 10:
                 model.T = 4
