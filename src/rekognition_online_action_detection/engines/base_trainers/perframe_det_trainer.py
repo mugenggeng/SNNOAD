d6d1e0e4b8f7232e0a9a78d5016321c6252f0698
mugenggeng@163.com <mugenggeng@163.com>
2025-02-12 10:32:51 +0800

    fusion

--- src/rekognition_online_action_detection/engines/base_trainers/perframe_det_trainer.py
+++ src/rekognition_online_action_detection/engines/base_trainers/perframe_det_trainer.py
@@ -107,8 +107,8 @@ class MACACCalculator:
         """处理卷积/全连接层的MAC计算"""
         if isinstance(module, nn.Conv1d):
             # [T, B, C_in, L_in] -> [T, B, C_out, L_out]
-            print(type(input))
-            print(input[0].shape)
+            # print(type(input))
+            # print(input[0].shape)
             T_B, C_in, L_in = input[0].shape
             T ,B = 4, T_B//4
             C_out = module.out_channels
