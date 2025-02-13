e3e48c25ed5e522ecd24171158b563d363e4b3da
mugenggeng@163.com <mugenggeng@163.com>
2025-02-13 22:53:00 +0800

    fusion

--- src/rekognition_online_action_detection/models/transformer/snn_module.py
+++ src/rekognition_online_action_detection/models/transformer/snn_module.py
@@ -161,11 +161,11 @@ class RepConv(nn.Module):
         conv1x1 = nn.Conv1d(in_channel, in_channel, 1, 1, 0, bias=False, groups=1)
         bn = BNAndPadLayer(pad_pixels=1, num_features=in_channel)
         conv3x3 = nn.Sequential(
-            nn.Conv1d(in_channel, in_channel, 3, 1, 0, groups=in_channel, bias=False),
+            nn.Conv1d(in_channel, in_channel, 3, 1, 0, groups=in_channel//4, bias=False),
             nn.Conv1d(in_channel, out_channel, 1, 1, 0, groups=1, bias=False),
             nn.BatchNorm1d(out_channel),
         )
-        # self.shortcut = nn.Conv1d(in_channel, out_channel, 1) if in_channel != out_channel else nn.Identity()
+        self.shortcut = nn.Conv1d(in_channel, out_channel, 1) if in_channel != out_channel else nn.Identity()
 
         self.body = nn.Sequential(conv1x1, bn, conv3x3)
         for m in self.modules():
@@ -178,7 +178,7 @@ class RepConv(nn.Module):
                     nn.init.constant_(m.bias, 0.5)  # 偏置项非零初始化
 
     def forward(self, x):
-        return self.body(x)
+        return self.body(x) + self.shortcut(x)
 
 
 class SepConv(nn.Module):
